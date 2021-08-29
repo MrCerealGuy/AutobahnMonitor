3c4dbb8d5b0f56268fd56a25b6a6c8362f249f97
Andreas Zahnleiter <mrcerealguy@gmx.de>
2021-08-29 15:11:03 +0200

    Moved MyGlobals to AutobahnMonitor.Globals.cs

---
+++ AutobahnMonitor/AutobahnMonitor.Globals.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.IO;
+using System.Net;
+
+using AutobahnMonitor.WebAPIHelper;
+using static AutobahnMonitor.WebAPIHelper.MyGlobals;
+
+namespace AutobahnMonitor
+{
+    public static partial class MyGlobals
+    {
+        //readonly List<string> sources = new List<string> {
+        //    "Autobahnen", "Baustellen", "Staus", "Rastplätze", "Webcams", "Verkehrsmeldungen", "Sperrungen", "Ladestationen" };
+
+        public static readonly string serverUri = "https://verkehr.autobahn.de/o/autobahn";
+
+        public static JSONRoads jsonDeserializedRoads;
+        public static JSONRoadworks jsonDeserializedRoadworks;
+        public static JSONWebcams jsonDeserializedWebcams;
+
+        public static readonly Dictionary<string, AutobahnAPIService> services = new Dictionary<string, AutobahnAPIService> {
+            { "Webcams", new AutobahnAPIService()
+                {
+                    deDescription = "Webcams",
+                    enDescription = "webcam",
+                    serviceUri = "/{roadId}/services/webcam",
+                    detailsUri = "/details/webcam/{webcamId}"
+                }
+            },
+            { "Baustellen", new AutobahnAPIService()
+                {
+                    deDescription = "Baustellen",
+                    enDescription = "roadworks",
+                    serviceUri = "/{roadId}/services/roadworks",
+                    detailsUri = "/details/roadworks/{roadworkId}"
+                }
+            },
+        };
+    }
+}
--- AutobahnMonitor/AutobahnMonitor.cs
+++ AutobahnMonitor/AutobahnMonitor.cs
@@ -7,42 +7,8 @@ using System.Net.Http;
 using System.IO;
 using System.Net;
 
-using AutobahnMonitor.WebAPIHelper;
-using static AutobahnMonitor.WebAPIHelper.MyGlobals;
-
 namespace AutobahnMonitor
 {
-    public static partial class MyGlobals
-    {
-        //readonly List<string> sources = new List<string> {
-        //    "Autobahnen", "Baustellen", "Staus", "Rastplätze", "Webcams", "Verkehrsmeldungen", "Sperrungen", "Ladestationen" };
-
-        public static readonly string serverUri = "https://verkehr.autobahn.de/o/autobahn";
-       
-        public static JSONRoads jsonDeserializedRoads;
-        public static JSONRoadworks jsonDeserializedRoadworks;
-        public static JSONWebcams jsonDeserializedWebcams;
-
-        public static readonly Dictionary<string, AutobahnAPIService> services = new Dictionary<string, AutobahnAPIService> {
-            { "Webcams", new AutobahnAPIService()
-                {
-                    deDescription = "Webcams",
-                    enDescription = "webcam",
-                    serviceUri = "/{roadId}/services/webcam",
-                    detailsUri = "/details/webcam/{webcamId}"
-                }
-            },
-            { "Baustellen", new AutobahnAPIService()
-                {
-                    deDescription = "Baustellen",
-                    enDescription = "roadworks",
-                    serviceUri = "/{roadId}/services/roadworks",
-                    detailsUri = "/details/roadworks/{roadworkId}"
-                }
-            },
-        };
-    }
-   
     static class Program
     {
         /// <summary>
